39041f1eb073ab4a3b1b9409bad60232b1022825
Ruben Borges <ruben.gyn@gmail.com>
2025-12-02 14:18:01 -0300

    back:fix: ajusta CrossOrigins para front conectar

--- back/src/main/java/br/com/ruben/desafionfs/controller/CreditoController.java
+++ back/src/main/java/br/com/ruben/desafionfs/controller/CreditoController.java
@@ -4,13 +4,11 @@ import br.com.ruben.desafionfs.model.Credito;
 import br.com.ruben.desafionfs.service.CreditoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/creditos")
 public class CreditoController {
